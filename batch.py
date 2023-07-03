262ad29e5bac841762ccebcd915fb28c9f8b0cd5
tommasosassoli <tommaso.sassoli@stud.unifi.it>
2023-07-03 15:03:48 +0200

    logging and exception handling

--- batch.py
+++ batch.py
@@ -1,3 +1,6 @@
+import logging
+logging.basicConfig(filename='log/batch.log', level=logging.INFO)
+
 from art.analysis import Analyzer
 from art.data import get_artwork_list
 from db.utils import select, insert
@@ -6,13 +9,19 @@ from tqdm import tqdm
 
 def batch():
     artwork_list = get_artwork_list()
+    logging.info('Start batch process')
     for art in tqdm(artwork_list):
         segments = select(art)
         if segments is None:
-            # make the analysis and insert into db
-            analyzer = Analyzer(art)
-            segments = analyzer.analyze()
-            insert(art, segments)
+            try:
+                # make the analysis and insert into db
+                logging.info('Analyzing ' + art.get_title())
+                analyzer = Analyzer(art)
+                segments = analyzer.analyze()
+                insert(art, segments)
+            except Exception as exc:
+                logging.error('Exception: ' + str(exc))
+    logging.info('End batch process')
 
 
 if __name__ == "__main__":
